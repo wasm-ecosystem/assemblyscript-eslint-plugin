21732a11e0bd9ab69b8c2a5815bea7597e0b8014
meowbmw <wang.feng@partner.bmw.com>
2025-06-06 17:40:22 +0800

    clean up test case

--- tests/rules/noRepeatedMemberAccess.test.ts
+++ tests/rules/noRepeatedMemberAccess.test.ts
@@ -59,13 +59,6 @@ describe("Rule: no-spread", () => {
       export const banana = Juice.D32
       export const cat = Juice.D33
       `,
-        `
-export namespace Constants {
-    export const defaultDebounceTimeRegular_s: u32 = SendTime.NEXT_REGULAR
-    export const debounceTimeGnssInnerCity_s = SendTime.S30
-    export const debounceTimeGnssMotorway_s = SendTime.S120
-}
-      `,
         /**
          * WARN: should NOT extract [] elements as they can get modified easily
          * This is implemented by detecting brackets "[]" in chains
@@ -81,12 +74,6 @@ export namespace Constants {
               data[0][1].count++;
               send(data[0][1].id);
             `,
-        `
-          const a = dataset[0][1].x + dataset[0][1].y;
-          dataset[0][1].update();
-          const b = dataset[0][1].z * 2;
-          notify(dataset[0][1].timestamp);
-        `,
         // WARN: DONT extract when function with possible side effect is called upon
         `
           const a = data.x + data.y;
@@ -95,77 +82,10 @@ export namespace Constants {
           notify(data.x);
         `,
         `
-          const first = data.items[0].config['security'].rules[2].level;
-          data.items[0].config['security'].rules[2].enabled = true;
-          validate(data.items[0].config['security'].rules[2].level);
-        `,
-        `
     const v1 = obj[123].value;
     const v2 = obj[123].value;
     const v3 = obj[123].value;
   `,
-        // some more complex cases
-        `
-        
-      /**
-       * Distance-based emission strategy
-       */
-      export class DistanceBasedDeliveryStrategy implements EmissionStrategy {
-        private checkStateChange(attr: NumericProperty): bool {
-          return (
-            attr.getCurrent().isUnstable() &&
-            attr.getPrevious() != null &&
-            attr.getPrevious()!.isStable()
-          );
-        }
-      
-        public isEqual(other: GenericValueContainer<i32>): bool {
-          return (
-            this.isStable() == other.isStable() &&
-            this.isActive() == other.isActive() &&
-            (!this.isActive() || this.getValue() == other.getValue())
-          );
-        }
-      
-        public copy(): GenericValueContainer<i64> {
-          const clone = new LongValueWrapper(this.initializer);
-          clone.status = this.status;
-          clone.error = this.error;
-          return clone;
-        }
-      }`,
-        `
-      
-      let activeConfig: BaseConfiguration;
-      const configData = AppContext.loadSettings();
-      
-      if (configData) {
-        activeConfig = configData;
-      } else {
-        activeConfig = new BaseConfiguration();
-        activeConfig.initializationDelay = Constants.DEFAULT_INIT_DELAY;
-        activeConfig.fastPollingInterval = Constants.DEFAULT_FAST_INTERVAL;
-        activeConfig.normalPollingInterval = Constants.DEFAULT_NORMAL_INTERVAL;
-      }
-      
-      if (runtimeEnv && eventController) {
-        SystemHookManager.instance = this;
-        this.eventController = eventController;
-        runtimeEnv.registerStateChangeListener(SystemHookManager.handleStateChange);
-        this.lastKnownState = runtimeEnv.getCurrentEnvironmentState();
-      }
-      `,
-        `
-      if (
-        currentSession.authType == AuthType.PRIVILEGED &&
-        currentSession.status == SessionStatus.ACTIVE &&
-        this.runtimeEnv.getCurrentEnvironmentState().isPresent()
-      ) {
-        const timestamp = getTimestamp();
-        serviceInstance.lastSyncTime = timestamp;
-      }
-      
-                  `,
         // shouldn't report when modified
         `
         const v1 = a.b.c;
@@ -179,12 +99,6 @@ export namespace Constants {
         const v2 = a.b.c; 
         const v3 = a.b.c; 
         `,
-        `
-            this.vehicleSys!.automobile = new TransportCore(new TransportBlueprint());
-            this.vehicleSys!.automobile!.underframe = new ChassisAssembly(new ChassisSchema());
-            this.vehicleSys!.automobile!.underframe!.propulsionCover = new EngineEnclosure(new EnclosureSpec());
-            this.vehicleSys!.automobile!.underframe!.logisticsBay = new CargoModule(new ModuleTemplate());
-            `,
       ],
 
       invalid: [
